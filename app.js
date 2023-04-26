11074b699f6f6aaa6d90c384b9a1279f9e0ed779
Olga Elyakova <alolelyc@yandex.ru>
2023-04-26 21:35:24 +0300

    2

--- app.js
+++ app.js
@@ -14,10 +14,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('*', (req, res) => {
-  res.status(ERR_STATUS_NOT_FOUND_404).send({ message: 'Данный URL не существует' });
-});
 
 app.use((req, res, next) => {
   req.user = {
@@ -29,6 +25,10 @@ app.use((req, res, next) => {
 
 app.use('/users', users);
 app.use('/cards', cards);
+app.use(express.urlencoded({ extended: true }));
+app.use('*', (req, res) => {
+  res.status(ERR_STATUS_NOT_FOUND_404).send({ message: 'Данный URL не существует' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
