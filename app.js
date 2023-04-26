0265d9cc4b61945db13f396b496f39f15af1ef21
Olga Elyakova <alolelyc@yandex.ru>
2023-04-26 17:57:34 +0300

    ПР13 замена порта

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const cards = require('./routes/cards');
 const users = require('./routes/users');
 
-const { PORT = 3005 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
